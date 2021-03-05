bf070d4d699438a045224dac264586d74cb2d5c0
alexdali <alexdali@gmail.com>
2021-03-05 10:21:35 +0400

    initial commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c78dc21e3e07e66b14a58f715f916748'),
+    },
+  },
+});
